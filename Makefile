0c60028be35d828a49abbef53c5c7776d6064072
Santiago Medina <devsanmedina@protonmail.com>
2022-03-23 22:11:28 -0500

    feat: Download grossing list

--- Makefile
+++ Makefile
@@ -4,3 +4,16 @@ venv: .venv
 	python3 -m venv .venv
 	.venv/bin/pip install -U pip setuptools wheel
 	.venv/bin/pip install pip-tools
+
+compile.in:
+	.venv/bin/pip-compile -r requirements.in
+
+sync:
+	.venv/bin/pip-sync
+
+format:
+	.venv/bin/black .
+	.venv/bin/isort .
+
+data/list:
+	.venv/bin/python src/download_list.py
---
+++ src/download_list.py
@@ -0,0 +1,35 @@
+import time
+from pathlib import Path
+
+import requests
+
+TOP_GROSSING_URL = "https://www.boxofficemojo.com/chart/ww_top_lifetime_gross/"
+
+
+def main():
+    data_dir_path = Path(__file__).parent.parent / "data" / "list"
+    if not data_dir_path.is_dir():
+        data_dir_path.mkdir(parents=True)
+
+    for offset in [0, 200, 400, 600, 800]:
+        offset_param = "" if offset == 0 else f"?offset={offset}"
+        url = f"{TOP_GROSSING_URL}{offset_param}"
+        response = requests.get(url)
+        if not response.ok:
+            raise Exception(
+                "Server did not answered an OK status. Answer was:\n"
+                f"URL: {url}\n"
+                f"Status: {response.status_code}\n"
+                f"Body: {response.content}"
+            )
+
+        offset_html_path = data_dir_path / f"offset-{offset}.html"
+        with offset_html_path.open(mode="wb") as offset_fp:
+            offset_fp.write(response.content)
+
+        print(f"{url} downloaded. Waiting 2 secs...")
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
